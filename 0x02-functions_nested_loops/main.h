73a3b70fc099ab82b1bf13d27ebd57845ee8b8bc
Oso Olamide <olamideoso65@gmail.com>
2022-03-15 10:45:14 -0700

    This program prints _putchar followed by a new line

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,23 +1,13 @@
-#include "main.h"
-
-/**
- * main - check the code for ALX School students
- *
- * program that prints _putchar, followed by a new line
- *
- * Return: always 0 (success)
- */
-
-int main(void)
-{
-        char s[] = "_putchar";
-        int i = 0;
-
-        while (s[i] != '\0')
-        {
-                _putchar(s[i]);
-                i++;
-        }
-        _putchar(10);
-        return (0);
-}
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
